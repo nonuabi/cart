8a59b641f559ddba40e236ac03453f76bb62b9d4
nonuabi <nonuabi@gmail.com>
2021-06-03 15:15:47 +0530

    added component , css folder

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./css/index.css";
+import App from "./Components/App";
 import firebase from "firebase";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  
+  apiKey: "AIzaSyDbJbVZuUSHZK_2--6MTXRW4axRiAR8joA",
+  authDomain: "cart-75c93.firebaseapp.com",
+  projectId: "cart-75c93",
+  storageBucket: "cart-75c93.appspot.com",
+  messagingSenderId: "163251411836",
+  appId: "1:163251411836:web:c3d08e455f08e3988662f9",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
